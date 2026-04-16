846ea0fc6848466554780ee5caf32c6054426677
tuistbot <noreply@tuist.dev>
2026-04-16 16:01:37 +0000

    chore(release): v0.1.2

--- checksum-Elixir.Javex.Native.exs
+++ checksum-Elixir.Javex.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libjavex_nif-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:eaa1c28ead9062272d2f1b3f00a1bcbb5a6762f92ed7abbd5acfe6f1e8151b51",
-  "libjavex_nif-v0.1.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:87ecd82df3246fe685866fdbe1a743a04df93acf41c2e36844ffe2f722944b96",
-  "libjavex_nif-v0.1.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:5f317d3929d7858ceb692ad203cf1ce54aeec0120575c0ee8b45584321af1166",
-  "libjavex_nif-v0.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7007ab1c5f4a3205bd6afd24ff66bb76d464a2ccaa9618b2379c28d26a3863f8",
+  "libjavex_nif-v0.1.2-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:552b547f139b0e7866e54309c97383651dd5cf20d92e28201fc99a0bb8a738dd",
+  "libjavex_nif-v0.1.2-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:2f3dbb208fb163e552c38ca95ca83839dc1f9d5ef5e60872b2749c59387412c4",
+  "libjavex_nif-v0.1.2-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:161440dd37f94cf2ae33ba51d27860e613b193c2dcd0b2679246e7df6738f72b",
+  "libjavex_nif-v0.1.2-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4491155b05c0245baec9b12bdc36f70663e11a2ef1c931e3e06dd0f01e0a81c7",
 }
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Javex.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/tuist/javex"
 
   def project do
